4839c33eb6f437420b30de7a396e80343b7d7508
eagleking <805575462@qq.com>
2024-06-18 20:44:42 +0800

    更新readme，增加adai lr=0.5

--- readme.md
+++ readme.md
@@ -1,7 +1,33 @@
+
 本项目比较了Adam、Adai、AdaptiveLR-Adai和SGDM四种优化算法的性能。
 
 
-模型采用的是ResNet-18，batchsize为128，数据集为CIFAR-10，pkl中保存了这几种优化算法训练100个epoch的accuracy的list。
+模型采用的是ResNet-18，损失函数为交叉熵损失函数，batchsize为128，数据集为CIFAR-10。
 
 
 Adai算法基于论文Adaptive Inertia:Disentangling the Effects of Adaptive Learning Rate and Momentum。AdaptiveLR-Adai是我自己提出的将Adam的自适应学习率加入Adai的新算法。
+
+## 超参数
+Adam
+|学习率|一阶矩估计衰减率$\beta_1$|二阶矩估计衰减率$\beta_2$|数值稳定性参数 $\epsilon$|weight decay|
+|---|---|---|---|---|
+|0.001|0.9|0.999|1e-8|0|
+
+Adai
+|学习率|自适应动量调整参数$\beta_0$|二阶矩估计衰减率$\beta_2$|$\beta_1$截断参数 $\epsilon$|
+|---|---|---|---|
+|1.0和0.5|0.1|0.99|1e-3|
+
+AdaptiveLR-Adai
+|学习率|自适应动量调整参数$\beta_0$|二阶矩估计衰减率$\beta_2$|数值稳定性参数 $\epsilon$|
+|---|---|---|---|
+|0.001|0.1|0.99|1e-3|
+
+SGDM
+|学习率|动量衰减率|weight decay|
+|---|---|---|
+|0.001|0.9|0|
+
+![img.png](img.png)
+
+![img_1.png](img_1.png)
--- visual.py
+++ visual.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import pickle
-adam_file = open('adam_100_epochs_lr=0.001.pkl', 'rb')
-adai_file = open('adai_100_epochs_lr=1.0.pkl', 'rb')
-adai_adaptivelr_file = open('adai_adlr_100_epochs_lr=0.001.pkl', 'rb')
-sgdw_file = open('sgdm_100_epochs_lr=0.001.pkl', 'rb')
+adam_file = open('Adam_accuracy_200_epochs_lr=0.001.pkl', 'rb')
+adai_file = open('Adai_accuracy_200_epochs_lr=1.0.pkl', 'rb')
+adai05_file = open("Adai_accuracy_200_epochs_lr=0.5.pkl", 'rb')
+adai_adaptivelr_file = open('Adai_Adlr_accuracy_200_epochs_lr=0.001.pkl', 'rb')
+sgdw_file = open('SGDM_accuracy_200_epochs_lr=0.001.pkl', 'rb')
+
 y_adai_accuracy = pickle.load(adai_file)
+y_adai05_accuracy = pickle.load(adai05_file)
 y_adam_accuracy = pickle.load(adam_file)
 y_adai_adaptivelr_accuracy = pickle.load(adai_adaptivelr_file)
 y_sgdw_accuracy = pickle.load(sgdw_file)
@@ -20,9 +23,40 @@ plt.xlabel('epoch')    # x轴标签
 plt.ylabel('accuracy(%)')     # y轴标签
 
 plt.plot(epoch, y_adam_accuracy, linewidth=1, linestyle="solid", label="adam", color='red')
-plt.plot(epoch, y_adai_accuracy, linewidth=1, linestyle="solid", label="adai", color='blue')
+plt.plot(epoch, y_adai_accuracy, linewidth=1, linestyle="solid", label="adai lr=1.0", color='blue')
 plt.plot(epoch, y_adai_adaptivelr_accuracy, linewidth=1, linestyle="solid", label="adai_adlr", color='purple')
 plt.plot(epoch, y_sgdw_accuracy, linewidth=1, linestyle="solid", label="sgdw", color='green')
+plt.plot(epoch, y_adai05_accuracy, linewidth=1, linestyle="solid", label="adai lr=0.5", color='pink')
 plt.legend()
 plt.title('accuracy curve')
+plt.show()
+
+adam_loss_file = open('Adam_loss_200_epochs_lr=0.001.pkl', 'rb')
+adai_loss_file = open('Adai_loss_200_epochs_lr=1.0.pkl', 'rb')
+adai05_loss_file = open("Adai_loss_200_epochs_lr=0.5.pkl", 'rb')
+adai_adaptivelr_loss_file = open('Adai_Adlr_loss_200_epochs_lr=0.001.pkl', 'rb')
+sgdm_loss_file = open('SGDM_loss_200_epochs_lr=0.001.pkl', 'rb')
+
+y_adai_loss = pickle.load(adai_loss_file)
+y_adai05_loss = pickle.load(adai05_loss_file)
+y_adam_loss = pickle.load(adam_loss_file)
+y_adai_adaptivelr_loss = pickle.load(adai_adaptivelr_loss_file)
+y_sgdm_loss = pickle.load(sgdm_loss_file)
+
+plt.figure()
+# 去除顶部和右边框框
+ax = plt.axes()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.xlabel('epoch')    # x轴标签
+plt.ylabel('loss')     # y轴标签
+
+plt.plot(epoch, y_adam_loss, linewidth=1, linestyle="solid", label="adam", color='red')
+plt.plot(epoch, y_adai_loss, linewidth=1, linestyle="solid", label="adai lr=1.0", color='blue')
+plt.plot(epoch, y_adai_adaptivelr_loss, linewidth=1, linestyle="solid", label="adai_adlr", color='purple')
+plt.plot(epoch, y_sgdm_loss, linewidth=1, linestyle="solid", label="sgdw", color='green')
+plt.plot(epoch, y_adai05_loss, linewidth=1, linestyle="solid", label="adai lr=0.5", color='pink')
+plt.legend()
+plt.title('loss curve')
 plt.show()
